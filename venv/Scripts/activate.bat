b071e97d532c305bee7595bd7d63ef5794676249
Mav023 <gabrielluismaverick@gmail.com>
2023-07-26 22:54:10 +0800

    publisher analytics

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\luis\Desktop\Programs\Data-Science-Projects\VIdeogame_Sales\venv
+set VIRTUAL_ENV=C:\Users\luis\Desktop\Programs\Data-Science-Projects\Videogame-Sales-EDA\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
